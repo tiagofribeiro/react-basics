8b34203bc4cacdcfba8a524439b0482195756007
tiagofribeiro <tg.frnds@outlook.com>
2023-08-17 04:12:44 -0300

    Props & mapping

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,22 @@
 import './ExpenseItem.css';
 
-export default function ExpenseItem() {
+export default function ExpenseItem({ date, title, amount }) {
+    const day = date.toLocaleString("pt-BR", { day: "2-digit" });
+    const month = date.toLocaleString("pt-BR", { month: "long" });
+    const year = date.getFullYear();
+
     return (
         <div className='expense-item'>
-            <div>16/08/2023</div>
+            <div>
+                <div>{day}</div>
+                <div>{month}</div>
+                <div>{year}</div>
+            </div>
             <div className='expense-item__description'>
-                <h2>Mercado</h2>
-                <div className='expense-item__price'>R$200,50</div>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>
+                    R$ {amount}
+                </div>
             </div>
         </div>
     );
